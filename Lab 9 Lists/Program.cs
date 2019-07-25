8a987d729072a7c9caf296c8afcff9c5e48cbc3a
myerscody16 <myerscody16@live.com>
2019-07-24 22:44:18 -0400

    redone entirely, looking great

--- Lab 9 Lists/Program.cs
+++ Lab 9 Lists/Program.cs
@@ -1,160 +1,187 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
-
-namespace Lab_9_Lists
+namespace Lab_8_Get_to_know_your_classmates
 {
     class Program
     {
         static void Main(string[] args)
         {
             bool cont = true;
-            while(cont)
-            { 
-             
-                List<string> studentNames = new List<string>
-                {
-                    "Alex", "Bailey", "Evan", "John", "Langston"
-                };
-
-                List<string> studentFood = new List<string>
-                {
-                    "Pizza","Hot Dog","Hamburger","Cheeseburger","milkshake"
-                };
-
-                List<string> studentHometown = new List<string>
-                {
-                    "Detroit", "Berkley","Des Moines", "Los Angeles","Southfield"
-                };
-
-                List<string> studentFavColor = new List<string>
-                {
-                    "red", "blue", "green", "yellow", "purple"
-                };
-
-                int studentNum = GetStudentNum("Please input the number corresponding to the student you'd like to know more about \n 1. Alex \n 2. Bailey \n 3. Evan \n 4. John \n 5. Langston");
-                string studentName = GetStudentName(studentNum, studentNames);
-                Console.WriteLine($"What would you like to know about {studentName}? Please input the corresponding value. \n 1. Favorite food \n 2.Hometown \n 3. Favorite Color \n 4. All of the above");
-                string foodChoice = "";
-                string hometown = "";
-                string colorChoice = "";
-                bool cont2 = true;
-                while (cont2)
-                {
-                    try
-                    {
-                        int knowMore = int.Parse(Console.ReadLine());
-
-                        if (knowMore == 1)
-                        {
-                            foodChoice = studentFood[studentNum];
-                            cont2 = false;
-                        }
-                        else if (knowMore == 2)
-                        {
-                            hometown = studentHometown[studentNum];
-                            cont2 = false;
-                        }
-                        else if (knowMore == 3)
-                        {
-                            colorChoice = studentFavColor[studentNum];
-                            cont2 = false;
-                        }
-                        else if (knowMore == 4)
-                        {
-                            foodChoice = studentFood[studentNum];
-                            hometown = studentHometown[studentNum];
-                            colorChoice = studentFavColor[studentNum];
-                            cont2 = false;
-                        }
-                        else
-                        {
-                            Console.WriteLine("That was not a valid answer, please give a number between 1 and 4.");
-                            continue;
-                        }
-                    }
-                    catch
-                    {
-                        Console.WriteLine("That is not a valid response, please give a number between 1 and 4");
-                        continue;
-                    }
-                }
-            }
-
-            
-
-        }
-        public static int GetStudentNum(string message)
-        {
-            Console.WriteLine(message);
-            bool cont = true;
-            int studentNum = 0;
-            while(cont)
+            while (cont)
             {
-                string usrInput = Console.ReadLine();
+                Console.WriteLine("Please choose the corresponding number to the student you would like to learn more about:" + "\n" + "1. Dan" + "\n" + "2. Zack" + "\n" + "3. Evan" + "\n" + "4. Mitch" + "\n" + "5. Mallory");
+                string studentNum1 = Console.ReadLine();
+                int studentNum = 0;
                 try
                 {
-                    if (studentNum >= 1 && studentNum <= 5)
+                    studentNum = int.Parse(studentNum1);
+                    if (studentNum < 1 || studentNum > 5)
                     {
-                        studentNum = int.Parse(usrInput);
-                        cont = false;
-                    }
-                    else
-                    {
-                        Console.WriteLine("That is an invalid input, please choose one of the students numbers. [1-5]");
+                        Console.WriteLine("That was outside of the given range, let's try again.");
                         continue;
                     }
                 }
-                catch(FormatException)
+                catch (FormatException)
                 {
-                    Console.WriteLine("That is an invalid input, please choose one of the students numbers. [1-5]");
+                    Console.WriteLine("That was not valid, let's try again.");
+                    continue;
+                }
+
+                string studentName = ChooseStudent(studentNum - 1);
+                Console.WriteLine($"The student's name is {studentName}");
+
+                //asking to know favorite food
+                string studentFood = ChooseFood($"Would you like to know {studentName}'s favorite food?", (studentNum - 1));
+                if (studentFood != "")
+                {
+                    Console.WriteLine($"{studentName}'s favorite food is {studentFood}.");
+                }
+
+                //asking to know hometown
+                string studentHometown = ChooseHometown($"Would you like to know which town {studentName} grew up in?", (studentNum - 1));
+                if (studentHometown != "")
+                {
+                    Console.WriteLine($"{studentName}'s hometown is {studentHometown}.");
+                }
+
+                //asking to know favorite drink
+                string studentDrink = ChooseDrink($"Would you like to know {studentName}'s favorite drink?", (studentNum - 1));
+                if (studentDrink != "")
+                {
+                    Console.WriteLine($"{studentName}'s favorite drink is {studentDrink}.");
+                }
+
+                //asking to continue
+                cont = ChooseContinue();
+            }
+        }
+        public static string ChooseStudent(int studentNum)
+        {
+            List<string> nameList = new List<string> { "Dan", "Zack", "Evan", "Mitch", "Mallory" };
+            bool cont = true;
+            string studentName = "";
+            while (cont)
+            {
+                try
+                {
+                    studentName = nameList[studentNum];
+                    cont = false;
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("That was not valid, let's try again.");
                     continue;
                 }
             }
-            return studentNum;
-        }
-        public static string GetStudentName(int studentNum, List<string> studentNames)
-        {
-            string studentName = studentNames[studentNum];
             return studentName;
 
         }
-        public static int KnowMore(string message)
+        public static string ChooseFood(string message, int studentNum)
         {
             Console.WriteLine(message);
+            List<string> foodList = new List<string> { "hot dog", "cheeseburger", "milkshake", "curry", "tacos" };
+            string foodChoice = "";
             bool cont = true;
-            int choice = 0;
             while (cont)
             {
-                string usrInput = Console.ReadLine();
-                try
+                string response = Console.ReadLine().ToLower();
+                if (response == "y")
                 {
-                    choice = int.Parse(usrInput);
-                    if(choice >= 1 && choice <= 4)
-                    {
-                        cont = false;
-                    }
-                    else
-                    {
-                        Console.WriteLine("That was an invalid number, please input a number between 1 and 4.");
-                        continue;
-                    }
-                    
+                    foodChoice = foodList[studentNum];
+                    cont = false;
                 }
-                catch(FormatException)
+                else if (response == "n")
                 {
-                    Console.WriteLine("That was an invalid input, please input a number between 1 and 4.");
+                    cont = false;
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid response, let't try that again.");
                     continue;
                 }
-                
-                
+            }
+            return foodChoice;
+        }
+        public static string ChooseHometown(string message, int studentNum)
+        {
+            Console.WriteLine(message);
+            List<string> hometownList = new List<string> { "Detroit", "Chicago", "Boston", "Los Angeles", "Seattle" };
+            string hometown = "";
+            bool cont = true;
+            while (cont)
+            {
+                string response = Console.ReadLine().ToLower();
+                if (response == "y")
+                {
+                    hometown = hometownList[studentNum];
+                    cont = false;
+                }
+                else if (response == "n")
+                {
+                    cont = false;
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid response, let't try that again.");
+                    continue;
+                }
+            }
+            return hometown;
+        }
+        public static string ChooseDrink(string message, int studentNum)
+        {
+            Console.WriteLine(message);
+            List<string> drinkList = new List<string> { "Coke", "Pepsi", "Sprite", "Jeager", "Red-Bull" };
+            string drink = "";
+            bool cont = true;
+            while (cont)
+            {
+                string response = Console.ReadLine().ToLower();
+                if (response == "y")
+                {
+                    drink = drinkList[studentNum];
+                    cont = false;
+                }
+                else if (response == "n")
+                {
+                    cont = false;
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid response, let't try that again.");
+                    continue;
+                }
+            }
+            return drink;
+
+        }
+        public static bool ChooseContinue()
+        {
+            bool cont = true;
+            bool cont1 = true;
+            while (cont1)
+            {
+                Console.WriteLine("Would you like to learn about another student?");
+                string response = Console.ReadLine().ToLower();
+                if (response == "y")
+                {
+                    cont = true;
+                    cont1 = false;
+                }
+                else if (response == "n")
+                {
+                    cont = false;
+                    cont1 = false;
+                }
+                else
+                {
+                    Console.WriteLine("That was not a valid input, please try again.");
+                    continue;
+                }
 
             }
-            return choice;
-        }
-        public static string HandleDataChoice(int knowMoreNum)
-        {
+            return cont;
         }
     }
 }
