bf9913d93c5701b971647c6878ebd8c35d1fa701
myerscody16 <myerscody16@live.com>
2019-07-24 22:26:02 -0400

    Update, not finished

--- Lab 9 Lists/Program.cs
+++ Lab 9 Lists/Program.cs
@@ -9,30 +9,80 @@ namespace Lab_9_Lists
     {
         static void Main(string[] args)
         {
-            List<string> studentNames = new List<string>
-            {
-                "Alex", "Bailey", "Evan", "John", "Langston"
-            };
+            bool cont = true;
+            while(cont)
+            { 
+             
+                List<string> studentNames = new List<string>
+                {
+                    "Alex", "Bailey", "Evan", "John", "Langston"
+                };
 
-            List<string> studentFood = new List<string>
-            {
-                "Pizza","Hot Dog","Hamburger","Cheeseburger","milkshake"
-            };
+                List<string> studentFood = new List<string>
+                {
+                    "Pizza","Hot Dog","Hamburger","Cheeseburger","milkshake"
+                };
 
-            List<string> studentHometown = new List<string>
-            {
-                "Detroit", "Berkley","Des Moines", "Los Angeles","Southfield"
-            };
+                List<string> studentHometown = new List<string>
+                {
+                    "Detroit", "Berkley","Des Moines", "Los Angeles","Southfield"
+                };
 
-            List<string> studentColor = new List<string>
-            {
-                "red", "blue", "green", "yellow", "purple"
-            };
+                List<string> studentFavColor = new List<string>
+                {
+                    "red", "blue", "green", "yellow", "purple"
+                };
 
-            int studentNum = GetStudentNum("Please input the number corresponding to the student you'd like to know more about \n 1. Alex \n 2. Bailey \n 3. Evan \n 4. John \n 5. Langston");
-            string studentName = GetStudentName(studentNum, studentNames);
-            int knowMore = KnowMore($"What would you like to know about {studentName}? Please input the corresponding value. \n 1. Favorite food \n 2.Hometown \n 3. Favorite Color \n 4. All of the above");
-            string studentDataChoice = HandleDataChoice(knowMore);
+                int studentNum = GetStudentNum("Please input the number corresponding to the student you'd like to know more about \n 1. Alex \n 2. Bailey \n 3. Evan \n 4. John \n 5. Langston");
+                string studentName = GetStudentName(studentNum, studentNames);
+                Console.WriteLine($"What would you like to know about {studentName}? Please input the corresponding value. \n 1. Favorite food \n 2.Hometown \n 3. Favorite Color \n 4. All of the above");
+                string foodChoice = "";
+                string hometown = "";
+                string colorChoice = "";
+                bool cont2 = true;
+                while (cont2)
+                {
+                    try
+                    {
+                        int knowMore = int.Parse(Console.ReadLine());
+
+                        if (knowMore == 1)
+                        {
+                            foodChoice = studentFood[studentNum];
+                            cont2 = false;
+                        }
+                        else if (knowMore == 2)
+                        {
+                            hometown = studentHometown[studentNum];
+                            cont2 = false;
+                        }
+                        else if (knowMore == 3)
+                        {
+                            colorChoice = studentFavColor[studentNum];
+                            cont2 = false;
+                        }
+                        else if (knowMore == 4)
+                        {
+                            foodChoice = studentFood[studentNum];
+                            hometown = studentHometown[studentNum];
+                            colorChoice = studentFavColor[studentNum];
+                            cont2 = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("That was not a valid answer, please give a number between 1 and 4.");
+                            continue;
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("That is not a valid response, please give a number between 1 and 4");
+                        continue;
+                    }
+                }
+            }
+
+            
 
         }
         public static int GetStudentNum(string message)
@@ -105,7 +155,6 @@ namespace Lab_9_Lists
         }
         public static string HandleDataChoice(int knowMoreNum)
         {
-
         }
     }
 }
